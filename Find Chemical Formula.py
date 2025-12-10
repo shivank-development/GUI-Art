4558899a0013d6a97db1b563f47eb6acc3b15088
𝕊ℍ𝕀𝕍𝔸ℕ𝕂 <officialshivank01@gmail.com>
2025-12-10 06:45:03 +0530

    Chemical Repersentaion

---
+++ Find Chemical Formula.py
@@ -0,0 +1,16 @@
+import pubchempy as pcp
+
+# Ask user for a chemical name
+chemical_name = input("Enter chemical name: ")
+
+try:
+    # Fetch compound details from PubChem
+    compound = pcp.get_compounds(chemical_name, 'name')[0]
+
+    print(f"IUPAC Name: {compound.iupac_name}")
+    print(f"Common Name: {compound.synonyms[0]}")
+    print(f"Molecular Weight: {compound.molecular_weight}")
+    print(f"Formula: {compound.molecular_formula}")
+
+except IndexError:
+    print(f"No information found for '{chemical_name}'.")
