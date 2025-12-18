99a4378daef4d305461a31c049ae9303454d861f
𝕊ℍ𝕀𝕍𝔸ℕ𝕂 <officialshivank01@gmail.com>
2025-12-18 08:36:21 +0530

    Periodic Table Elements info

---
+++ Periodic Table Elements info.py
@@ -0,0 +1,10 @@
+import periodictable
+
+element = periodictable.Na
+
+print("Name:", element.name)
+print("Symbol:", element.symbol)
+print("Atomic Number:", element.number)
+print("Atomic Mass:", element.mass)
+print("Density (g/cm³):", element.density)
+print("Melting Point (K):", element.melting_point)
